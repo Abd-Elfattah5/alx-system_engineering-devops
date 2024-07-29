9844e863acfdf3e65f6049d47d85a717a8721146
wenny <abdelfatahwenny@yahoo.com>
2024-07-29 15:38:32 -0700

    write to a csv file from an API

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" a module that uses the request library
+to fetch data using the JSONPlaceholder API
+and put all the data inside a csv file
+"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    link = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    task_req = requests.get(link + "/todos")
+    usr_req = requests.get(link)
+
+    tasks_info = task_req.json()
+    usr_info = usr_req.json()
+    
+    headers = ["id", "name", "completed", "title"]
+
+    data = [{"id": usr_info["id"], "name": usr_info["name"],
+            "completed": task["completed"], "title": task["title"]}
+            for task in tasks_info]
+
+    file_name = "{}.csv".format(employee_id)
+
+    with open(file_name, mode='w', newline='', encoding="utf-8") as file:
+        writer = csv.DictWriter(file, fieldnames=headers)
+
+        writer.writerows(data)
